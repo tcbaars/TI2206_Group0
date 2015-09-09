3dafa35c7aa29db7648679194f658d5fc1f3a8b1
Boris Schrijver <boris@pcextreme.nl>
2015-09-09 23:22:04 +0200

    Made the movement of the player work accordingly.
    - Formatted a few classes.
    - Centered the screen on startup.

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -1,129 +1,128 @@
 package entities;
 
+import java.awt.Graphics2D;
+
 import animations.Animation;
 import enumerations.Direction;
 import handlers.OptionsHandler;
 
-import java.awt.Graphics2D;
-
 public class Player extends Entity {
 
+    private final double movespeed = 75;
 
-  private final double movespeed = 10;
+    private final double entitywidth = 100;
+    private final double entityheight = 50;
 
-  private final double entitywidth = 100;
-  private final double entityheight = 50;
+    private final double spritewidth = 300;
+    private final double spriteheight = 300;
+    private final String animationkey = "Player";
+    private final String animationurl = "/sprites/Player.png";
+    private final int numberframes = 2;
+    private final int animationdely = 10;
 
-  private final double spritewidth = 300;
-  private final double spriteheight = 300;
-  private final String animationkey = "Player";
-  private final String animationurl = "/sprites/Player.png";
-  private final int numberframes = 2;
-  private final int animationdely = 10;
+    private final double startscale = 20;
+    private final double targetscale = 100;
 
-  private final double startscale = 20;
-  private final double targetscale = 100;
+    private int currentScore;
+    private int numberFishEaten;
 
-  private int currentScore;
-  private int numberFishEaten;
-
-   /**
-    * Player.
-    */
-  public Player() {
-    super();
-    currentScore = 0;
-    numberFishEaten = 0;
-  }
-
-  @Override
-   protected void initialiseEntity() {
-    entityWidth = entitywidth;
-    entityHeight = entityheight;
-  }
-
-  @Override
-  protected void initialiseSprite() {
-    topLeftX = OptionsHandler.getInstance().getWidth() / 2;
-    topLeftY = OptionsHandler.getInstance().getHeight() / 2;
-    spriteWidth = spritewidth;
-    spriteHeight = spriteheight;
-    currentScale = startscale;
-    targetScale = targetscale;
-  }
-
-  @Override
-  protected Animation createAnimation() {
-    return Animation.createAnimation(animationkey, animationurl, numberframes, (int)spritewidth,
-        (int)spriteheight, animationdely);
-  }
-
-  @Override
-  protected void update() {}
-
-  @Override
-  protected void draw(Graphics2D graphic) {}
-
-  public void moveUp() {
-    topLeftY = 0;
-  }
-
-  public void moveDown() {
-    topLeftY = 50;
-  }
-
-  public void moveLeft() {
-    topLeftX = 0;
-  }
-
-  public void moveRight() {
-    topLeftX = 150;
-  }
-
-  /**
-   * Move.
-   */
-  public void move(Direction direction) {
-    switch (direction) {
-      case UP:
-        moveUp();
-        break;
-      case DOWN:
-        moveDown();
-        break;
-      case LEFT:
-        moveLeft();
-        break;
-      case RIGHT:
-        moveRight();
-        break;
-      default:
+    /**
+     * Player.
+     */
+    public Player() {
+        super();
+        currentScore = 0;
+        numberFishEaten = 0;
     }
-  }
 
-  /**
+    @Override
+    protected void initialiseEntity() {
+        entityWidth = entitywidth;
+        entityHeight = entityheight;
+    }
+
+    @Override
+    protected void initialiseSprite() {
+        topLeftX = OptionsHandler.getInstance().getWidth() / 2;
+        topLeftY = OptionsHandler.getInstance().getHeight() / 2;
+        spriteWidth = spritewidth;
+        spriteHeight = spriteheight;
+        currentScale = startscale;
+        targetScale = targetscale;
+    }
+
+    @Override
+    protected Animation createAnimation() {
+        return Animation.createAnimation(animationkey, animationurl, numberframes, (int) spritewidth,
+                (int) spriteheight, animationdely);
+    }
+
+    @Override
+    protected void update() {
+    }
+
+    @Override
+    protected void draw(Graphics2D graphic) {
+    }
+
+    public void moveUp() {
+        topLeftY -= movespeed;
+    }
+
+    public void moveDown() {
+        topLeftY += movespeed;
+    }
+
+    public void moveLeft() {
+        topLeftX -= movespeed;
+    }
+
+    public void moveRight() {
+        topLeftX += movespeed;
+    }
+
+    /**
+     * Move.
+     */
+    public void move(Direction direction) {
+        switch (direction) {
+        case UP:
+            moveUp();
+            break;
+        case DOWN:
+            moveDown();
+            break;
+        case LEFT:
+            moveLeft();
+            break;
+        case RIGHT:
+            moveRight();
+            break;
+        default:
+        }
+    }
+
+    /**
      * Assuming not null and consumable.
      */
-  @Override
-  protected void consume(Entity food) {
-    numberFishEaten++;
-    currentScore += food.consumedBy(this);
-  }
+    @Override
+    protected void consume(Entity food) {
+        numberFishEaten++;
+        currentScore += food.consumedBy(this);
+    }
 
-  @Override
-  protected int consumedBy(Entity eater) {
-    kill();
-    return 0;
-  }
+    @Override
+    protected int consumedBy(Entity eater) {
+        kill();
+        return 0;
+    }
 
-  public boolean isFull() {
-    return currentScale >= targetScale;
-  }
+    public boolean isFull() {
+        return currentScale >= targetScale;
+    }
 
-  public int getScore() {
-    return currentScore;
-  }
-
-
+    public int getScore() {
+        return currentScore;
+    }
 
 }
--- src/main/java/layers/Layer.java
+++ src/main/java/layers/Layer.java
@@ -1,133 +1,133 @@
 package layers;
 
-import enumerations.Key;
-import handlers.OptionsHandler;
-
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import enumerations.Key;
+import handlers.OptionsHandler;
+
 public abstract class Layer extends JPanel {
 
-  private Layer prev;
-  private Layer next;
+    private Layer prev;
+    private Layer next;
 
-  private boolean active;
-  private boolean visible;
+    private boolean active;
+    private boolean visible;
 
-  /**
-   * ContentLayer.
-   */
-  public Layer() {
-    super();
-    setPreferredSize( new Dimension(OptionsHandler.getInstance().getWidth(),
-        OptionsHandler.getInstance().getHeight()));
-    prev = null;
-    next = null;
-    active = true;
-    visible = true;
-  }
-
-  private void setPrevious(Layer prev) {
-    this.prev = prev;
-  }
-
-  /**
-   * Adds a layer to the image.
-   */
-  public void addLayer(Layer layer) {
-    this.next = layer;
-    if (layer != null) {
-      layer.setPrevious(this);
+    /**
+     * ContentLayer.
+     */
+    public Layer() {
+        super();
+        setPreferredSize(
+                new Dimension(OptionsHandler.getInstance().getWidth(), OptionsHandler.getInstance().getHeight()));
+        prev = null;
+        next = null;
+        active = true;
+        visible = true;
     }
-  }
 
-  /**
-   * Remove a layer.
-   */
-  public void removeLayer() {
-    if (prev != null) {
-      prev.addLayer(next);
+    private void setPrevious(Layer prev) {
+        this.prev = prev;
     }
-    prev = null;
-    next = null;
-  }
 
-  public boolean isActive() {
-    return active;
-  }
-
-  public void activate() {
-    active = true;
-  }
-
-  public void deactivate() {
-    active = false;
-  }
-
-  public boolean isVisible() {
-    return visible;
-  }
-
-  public void setVisible(boolean visible) {
-    this.visible = visible;
-  }
-
-  public void updateLayer() {
-    update();
-    updateNext();
-  }
-
-  protected abstract void update();
-
-  private void updateNext() {
-    if (next != null) {
-      next.updateLayer();
+    /**
+     * Adds a layer to the image.
+     */
+    public void addLayer(Layer layer) {
+        this.next = layer;
+        if (layer != null) {
+            layer.setPrevious(this);
+        }
     }
-  }
 
-  /**
-   * Draws the layer to the image.
-   */
-  public void drawLayer(Graphics2D graphic) {
-    if (visible) {
-      graphic = draw(graphic);
+    /**
+     * Remove a layer.
+     */
+    public void removeLayer() {
+        if (prev != null) {
+            prev.addLayer(next);
+        }
+        prev = null;
+        next = null;
     }
-    drawNext(graphic);
-  }
 
-  protected abstract Graphics2D draw(Graphics2D graphic);
-
-  private void drawNext(Graphics2D graphic) {
-    if (next != null) {
-      next.drawLayer(graphic);
+    public boolean isActive() {
+        return active;
     }
-  }
 
-  public void handleKeyPress(Key key) {
-    keyPressed(key);
-    passKeyPress(key);
-  }
-
-  public void handleKeyRelease(Key key) {
-    keyReleased(key);
-    passKeyRelease(key);
-  }
-
-  protected abstract void keyPressed(Key key);
-
-  protected abstract void keyReleased(Key key);
-
-  private void passKeyPress(Key key) {
-    if (next != null) {
-      next.handleKeyPress(key);
+    public void activate() {
+        active = true;
     }
-  }
 
-  private void passKeyRelease(Key key) {
-    if (next != null) {
-      next.handleKeyRelease(key);
+    public void deactivate() {
+        active = false;
     }
-  }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public void updateLayer() {
+        update();
+        updateNext();
+    }
+
+    protected abstract void update();
+
+    private void updateNext() {
+        if (next != null) {
+            next.updateLayer();
+        }
+    }
+
+    /**
+     * Draws the layer to the image.
+     */
+    public void drawLayer(Graphics2D graphic) {
+        if (visible) {
+            graphic = draw(graphic);
+        }
+        drawNext(graphic);
+    }
+
+    protected abstract Graphics2D draw(Graphics2D graphic);
+
+    private void drawNext(Graphics2D graphic) {
+        if (next != null) {
+            next.drawLayer(graphic);
+        }
+    }
+
+    public void handleKeyPress(Key key) {
+        keyPressed(key);
+        passKeyPress(key);
+    }
+
+    public void handleKeyRelease(Key key) {
+        keyReleased(key);
+        passKeyRelease(key);
+    }
+
+    protected abstract void keyPressed(Key key);
+
+    protected abstract void keyReleased(Key key);
+
+    private void passKeyPress(Key key) {
+        if (next != null) {
+            next.handleKeyPress(key);
+        }
+    }
+
+    private void passKeyRelease(Key key) {
+        if (next != null) {
+            next.handleKeyRelease(key);
+        }
+    }
 }
