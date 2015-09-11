b31d81290e6ef629fd8f0da1301d4d0128bb88a9
Adriaan De Vos <adriaan.devos+git@gmail.com>
2015-09-11 14:54:26 +0200

    Added instructions.

--- src/main/java/layers/InstructionsLayer.java
+++ src/main/java/layers/InstructionsLayer.java
@@ -1,30 +1,44 @@
 package layers;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
-
 import enumerations.Key;
 import handlers.FontOutlineHandler;
 
+import java.awt.*;
+
 public class InstructionsLayer extends Layer {
 
+    /*
+     * Appearance options of the title to be displayed
+     */
     private final String titletext = "INSTRUCTIONS";
     private final Color titlefill = Color.WHITE;
     private final Color titleoutline = Color.BLACK;
     private final float titleoutlinesize = 2;
     private final Font titlefont = new Font("Times New Roman", Font.BOLD, 100);
 
-    private final int ytitle = 125;
+    /*
+    * Appearance options of the text to be displayed
+    */
+    private final String[] text = {"Eat smaller fish to grow bigger", "Avoid larger fish",
+                            "The larger the fish, the more points", "you receive when eating"};
+    private final Font textfont = new Font("Times New Roman", Font.BOLD, 50);
 
+    /*
+     * The coordinates for the text
+     */
+    private final int ytitle = 125;
+    private final int yoption = 700;
+    private final int ytext = 200;
+    private final int ytextstep = 50;
+
+    /*
+     * Appearance options of the options to be displayed
+     */
     private final String optiontext = "BACK";
     private final Color selectedfill = Color.YELLOW;
     private final Color optionoutline = Color.BLACK;
     private final float optionoutlinesize = 1;
     private final Font optionfont = new Font("Times New Roman", Font.BOLD, 85);
-
-    private final int xoption = 300;
-    private final int yoption = 300;
 
     public InstructionsLayer(){
         super();
@@ -40,8 +54,21 @@ public class InstructionsLayer extends Layer {
 
     @Override
     public Graphics2D draw(Graphics2D graphic) {
-        FontOutlineHandler.drawTextCenterWidth(graphic, titlefont, titletext, titlefill, titleoutline, titleoutlinesize, ytitle);
-        FontOutlineHandler.drawText(graphic, optionfont, optiontext, selectedfill, optionoutline, optionoutlinesize, xoption, yoption);
+
+        // Title
+        FontOutlineHandler.drawTextCenterWidth(graphic, titlefont, titletext, titlefill, titleoutline,
+                                                titleoutlinesize, ytitle);
+
+        // Instructions
+        for (int i = 0; i < text.length; i++) {
+            FontOutlineHandler.drawTextCenterWidth(graphic, textfont, text[i], titlefill, optionoutline,
+                                                    optionoutlinesize, ytext + ytextstep * i);
+        }
+
+        // Back key
+        FontOutlineHandler.drawTextCenterWidth(graphic, optionfont, optiontext, selectedfill, optionoutline,
+                optionoutlinesize, yoption);
+
         return graphic;
     }
 
