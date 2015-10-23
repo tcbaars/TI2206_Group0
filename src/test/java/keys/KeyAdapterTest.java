ec8c530cb73bfb7fe005807dc81c83a69a062a3e
DylanStraub <denver_dersen@hotmail.com>
2015-10-23 22:12:14 +0200

    Update KeyAdapterTest.java
    < testing

--- src/test/java/keys/KeyAdapterTest.java
+++ src/test/java/keys/KeyAdapterTest.java
@@ -1,36 +1,47 @@
-//package keys;
-//
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-///**
-// * Created by Dylan 9-10-2015
-// * Test method for keys.KeyAdapter class.
-// */
-//public class KeyAdapterTest {
-//
-//    /**
-//     * This method tests the return value for convertkeypressed.
-//     */
-//    @Test
-//    public void testconvertKeyPressed() {
-//      assertEquals(GameKeys.UP,convertkeypressed(GameKeys.UP));
-//    }
-//
-//      /**
-//     * This method tests the return value for convertkeyreleased.
-//     */
-//    @Test
-//    public void testconvertKeyReleased() {
-//      assertEquals(GameKeys.UP,convertkeyreleased(GameKeys.UP));
-//    }
-//
-//      /**
-//     * This method tests the return value for convertkeypressed.
-//     */
-//    @Test
-//    public void testconvertKeyPressed() {
-//      assertEquals(GameKeys.MUSIC,convertkeyTyped(GameKeys.MUSIC));
-//    }
-//}
+package keys;
+
+import org.junit.Test;
+
+import enumerations.GameKeys;
+import keys.KeyAdapter;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+
+
+/**
+ * Created by Dylan 9-10-2015
+ * Test method for keys.KeyAdapter class.
+ */
+public class KeyAdapterTest {
+	KeyEvent key = new Mockito.mock(KeyEvent.class);
+	KeyEvent tkey = new Mockito.mock(KeyEvent.class);
+
+    /**
+     * This method tests if the output is a key and not null.
+     */
+	  @Test
+	    public void convertKeyPressedTest() {
+	        Mockito.when(key.getKeyCode()).thenReturn(GameKeys.UP.getKeyCode());
+	        assertEquals(GameKeys.UP, KeyAdapter.convertKeyPressed(key).getKey());
+	    }
+
+      /**
+     * This method tests if the output is a key and not null.
+     */
+    @Test
+    public void convertKeyReleasedTest() {
+    	 Mockito.when(key.getKeyCode()).thenReturn(GameKeys.UP.getKeyCode());
+    	 assertEquals(GameKeys.UP, KeyAdapter.convertKeyReleased(key).getKey());
+    }
+
+      /**
+     * This method tests if the output is a typedkey and not null.
+     */
+    @Test
+    public void convertKeyTypedTest() {
+    	Mockito.when(tkey.getKeyCode()).thenReturn(GameKeys.MUSIC.getKeyCode());
+        assertEquals(GameKeys.MUSIC, KeyAdapter.convertKeyPressed(key).getKey());
+    }
+}
