c16c6690948bd12827ddda7c75c377acebe967fe
DylanStraub <denver_dersen@hotmail.com>
2015-10-30 22:02:17 +0100

    Update BubbleSpawnerTest.java
    added updating test < testing

--- src/test/java/entityspawner/BubbleSpawnerTest.java
+++ src/test/java/entityspawner/BubbleSpawnerTest.java
@@ -38,4 +38,20 @@ public class BubbleSpawnerTest {
 	public void getEntitiesTest() {
 		assertTrue(bubblespawner.getEntities() != null);
 	}
+	
+	/**
+	 * this method tests the update method for looping the frames
+	 */
+	@Test
+	public void updateTest() {
+		for(int i = 0; i < 2000; i++) {
+			enemyspawner.update();
+		}
+		assertFalse(enemyspawner.getNumberEnemies() == 0);
+		enemyspawner.setFinalStage();
+		for(int i = 0; i < 6000; i++) {
+			enemyspawner.update();
+		}
+		assertTrue(enemyspawner.getNumberEnemies() == 0);
+	}
 }
